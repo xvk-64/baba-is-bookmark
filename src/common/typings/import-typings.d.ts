b853da7f1833df8281310470ccebbf126be30d72
Valen Kostich <valen.kostic@gmail.com>
2020-12-29 00:42:05 +1030

    Update asset import typings

---
+++ src/common/typings/import-typings.d.ts
@@ -0,0 +1,20 @@
+declare module "*.gif" {
+	const content: string;
+	export default content;
+}
+
+declare module "*.png" {
+	const content: string;
+	export default content;
+}
+
+declare module "*.svg" {
+	const content: any;
+	export default content;
+  }
+
+declare module "*.md" {
+	const content: string;
+	export default content;
+}
+
